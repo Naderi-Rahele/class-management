e9327558e760a7c5d020d818a04a2c64134b184a
miri <naderi.rahele@golrang.com>
2021-10-24 16:53:32 +0330

    all courses complate

--- src/app/all-courses/all-courses.component.ts
+++ src/app/all-courses/all-courses.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { Course, InMemoryDB, User } from '../services/in-memory-db';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-all-courses',
@@ -6,8 +10,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./all-courses.component.css']
 })
 export class AllCoursesComponent implements OnInit {
+  allCourses: Course[] = [];
 
-  constructor() { }
+  constructor(private service: UserService,
+    private toastService: MatSnackBar,
+    private router: Router) {
+    this.allCourses = this.service.getAllCourses();
+  }
+
+  selectCourse(courseId: number) {
+    if (this.service.getCurrentUser() === "") {
+      this.router.navigate(['/login']);
+      return;
+    }
+    this.service.addUsrCourse(courseId).subscribe(res => {
+      this.toastService.open('The course added successfuly', '', {
+        duration: 2000,
+        panelClass: ['bg-accent'],
+      });
+    }, error => {
+      this.toastService.open(error.message, '', {
+        duration: 2000,
+        panelClass: ['bg-warn'],
+      });
+    });
+  }
 
   ngOnInit(): void {
   }
