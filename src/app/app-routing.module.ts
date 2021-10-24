4f7335a205fe3dd6d1df8783927197d0af7a34a5
miri <naderi.rahele@golrang.com>
2021-10-24 16:58:56 +0330

    add all coursec route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AllCoursesComponent } from './all-courses/all-courses.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
 
 const routes: Routes = [
+  { path: '', component: HomeComponent },
   { path: 'register', component: RegistrationComponent },
-  { path: '', component: RegistrationComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'all-courses', component: AllCoursesComponent },
 ];
 
 @NgModule({
