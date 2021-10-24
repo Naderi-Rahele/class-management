66e76af4ffc30dc13706802d4ce88bdf7206f17e
miri <naderi.rahele@golrang.com>
2021-10-24 16:57:29 +0330

    add is login service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from './services/user.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'class-management';
+
+
+  constructor(private router: Router, private service: UserService) {
+  }
+
+  get isLoggedIn() {
+    return this.service.getCurrentUser();
+  }
+
+  login() {
+    this.router.navigate(['/login'])
+  }
+
+  logOut() {
+    this.service.logout().subscribe(res => {
+      this.router.navigate(['/login']);
+    });
+  }
+
+  register() {
+    this.router.navigate(['/register'])
+  }
 }
